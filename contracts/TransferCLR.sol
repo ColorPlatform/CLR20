fadf548cf578b64a1805c89cd54c0461fe2659a0
Nikolay Pakulin <nikolay@paxdatatech.com>
2020-12-11 16:12:40 +0900

    Added `destroy` to the contract

--- contracts/TransferCLR.sol
+++ contracts/TransferCLR.sol
@@ -4,19 +4,22 @@ import "./ColorCoin.sol";
 
 contract TransferCLR {
     ColorCoin public clr;
-    address internal owner;
+    address payable internal owner;
 
     constructor(ColorCoin addr) public {
         clr = addr;
         owner = msg.sender;
     }
 
+    /// @notice Transfers Color Coins from `from` wallet to the specified destination.
     function transfer(address from , address to, uint256 amount) public returns (bool) {
         require(msg.sender == owner, "Only owner can use this contract");
 
         return clr.transferFrom(from, to, amount);
     }
-    function transfer_Batch(address from , 
+
+    /// @notice Transfers Color Coins from `from` wallet to multiple destinations.
+    function transfer_Batch(address from, 
             address[] memory destinations, 
             uint256[] memory amounts
     ) public {
@@ -29,5 +32,11 @@ contract TransferCLR {
             uint256 amount = amounts[i];
             transfer(from, dst, amount);
         }
+    }
+
+    /// @notice Calls `selfdestruct` operator and transfers all Ethers to the contract owner
+    function destroy() public {
+        require(msg.sender == owner, "Only owner can destroy this contract");
+        selfdestruct(owner);
     }
 }
